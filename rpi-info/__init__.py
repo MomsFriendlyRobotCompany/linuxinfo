b779b9d753c7eca96a8b878e4bb7dc0d72f6754b
Kevin <walchko@users.noreply.github.com>
2019-10-27 21:18:24 -0400

    Update __init__.py

--- rpi-info/__init__.py
+++ rpi-info/__init__.py
@@ -1,8 +1,8 @@
 
-from .rpi2 import pi_info, PiInfo
+from .rpi2 import pi_info, RPiInfo
 from .linux import linux_info, LinuxInfo
 
 
 __author__ = "Kevin J. Walchko"
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
