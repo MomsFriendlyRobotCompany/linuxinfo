ed02d492ade3e0d3c6e3240b5cb8db8f3e9c1e79
walchko <walchko@users.noreply.github.com>
2020-02-14 07:07:47 -0500

    fix

--- linuxinfo/__init__.py
+++ linuxinfo/__init__.py
@@ -2,7 +2,7 @@ try:
     from importlib_metadata import version # type: ignore
 except ImportError:
     from importlib.metadata import version # type: ignore
-    
+
 # from .rpi import pi_info, RPiInfo, decode
 from .rpi import pi_info, RPiInfo
 from .linux import linux_info, LinuxInfo
@@ -10,4 +10,4 @@ from .linux import linux_info, LinuxInfo
 
 __author__ = "Kevin J. Walchko"
 __license__ = "MIT"
-__version__ = version("the_collector")
+__version__ = version("linuxinfo")
