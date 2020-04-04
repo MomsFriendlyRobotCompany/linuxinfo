7bb0e420f6f8896e8d7a3b1e5b95e2977b45fae3
Kevin <walchko@users.noreply.github.com>
2020-04-04 18:20:43 -0400

    Update rpi.py

--- linuxinfo/rpi.py
+++ linuxinfo/rpi.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2019 Kevin Walchko
+# see LICENSE for full details
+##############################################
 from collections import namedtuple
 import re
 from .helpers import read
